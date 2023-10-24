1b009804986e41e87d4cf6e4767e654fe32e2b66
SEF-Assessment4 <sheen2906@gmail.com>
2023-10-24 17:39:42 +1100

    Initial Commit

---
+++ SleepTracker/SleepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTrackerApp.swift
+//  SleepTracker
+//
+//  Created by apple on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SleepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
