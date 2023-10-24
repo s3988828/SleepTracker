a476b9ebb92a836d357a3f23122a3a6a3b1401dc
SEF-Assessment4 <sheen2906@gmail.com>
2023-10-24 20:19:23 +1100

    commit changes

--- SleepTrackerTests/SleepTrackerTests.swift
+++ SleepTrackerTests/SleepTrackerTests.swift
@@ -5,32 +5,46 @@
 //  Created by apple on 24/10/23.
 //
 
-import XCTest
 @testable import SleepTracker
+import XCTest
 
-final class SleepTrackerTests: XCTestCase {
+class SleepTrackerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    // Test if the function fetchIPAddress successfully fetches an IP address.
+    // Given the dependency on an external API, this test will essentially
+    // check if the API's response contains a valid IP format.
+    func testIPAddressFetching() {
+        let contentView = ContentView()
+        
+        let expectation = self.expectation(description: "Fetching IP Address")
+        
+        contentView.fetchIPAddress()
+        
+        // Provide ample time for the network request to complete.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+            if let ipAddress = contentView.ipAddress {
+                // Check if the IP address is in a valid format (simple regex for IP validation)
+                let ipPattern = "^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$"
+                XCTAssertTrue(ipAddress.range(of: ipPattern, options: .regularExpression) != nil, "Invalid IP format")
+            } else {
+                XCTFail("Failed to fetch IP address.")
+            }
+            expectation.fulfill()
         }
+        
+        waitForExpectations(timeout: 10.0, handler: nil)
+    }
+    
+    // Test if the SleepData's duration calculation is accurate.
+    // This will ensure that the time difference between the wakeTime and sleepTime
+    // is calculated correctly, which is essential for accurate reporting.
+    func testSleepDataDurationCalculation() {
+        let sleepTime = Date()
+        let wakeTime = sleepTime.addingTimeInterval(8*3600) // 8 hours later
+        let sleepData = SleepData(date: Date(), sleepTime: sleepTime, wakeTime: wakeTime)
+        
+        // The expected duration is 8 hours, so we check if it matches.
+        XCTAssertEqual(sleepData.duration, 8.0, accuracy: 0.01, "Sleep duration calculation is inaccurate.")
     }
 
 }
